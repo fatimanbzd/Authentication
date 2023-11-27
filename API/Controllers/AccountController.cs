b6b9d947432a8bf42db35fde6bb18542e75734a7
f.nabizadeh <fatemenabizade@hotmail.com>
2023-11-27 08:50:27 +0330

    implement srvice jwt

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using API.DTOs.Account;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/Account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JWTService _jwtService;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(JWTService jwtService, SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _jwtService = jwtService;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async  Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user == null) return Unauthorized("Invalid Username or Password");
+
+            if (user.EmailConfirmed == false) return Unauthorized("Please Confirm your email");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (!result.Succeeded) return Unauthorized("Invalid Username or Password");
+
+            return CreateApplicationUserDto(user);
+
+        }
+
+        #region private Helper Methods
+        private UserDto CreateApplicationUserDto(User user)
+        {
+            return new UserDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                JWT = _jwtService.CreateJWT(user),
+            };
+        }
+        #endregion
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,15 @@
+using API.Data;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +19,42 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<JWTService>();
+
+builder.Services.AddIdentityCore<User>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false; ;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+
+    options.SignIn.RequireConfirmedEmail = true;
+})
+    .AddRoles<IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddEntityFrameworkStores<Context>()
+    .AddSignInManager<SignInManager<User>>()
+   .AddUserManager<UserManager<User>>()
+   .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+            ValidIssuer = builder.Configuration["JWT:Isuuser"],
+            ValidateIssuer = true,
+            ValidateAudience = true
+        };
+});
 
 var app = builder.Build();
 
@@ -21,6 +66,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
